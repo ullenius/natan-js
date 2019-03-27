b3d0eb3eb9d12b1d2edeafe882004ddbee53dab3
Anosh D. Ullenius <anosh@anosh.se>
2019-03-27 21:42:22 +0100

    Adding area for question to be displayed

--- scripts/game.js
+++ scripts/game.js
@@ -6,17 +6,29 @@ var calculator = {
 
 function addDigit(number) {
   calculator.userInput.push(number);
+  addToTextbox(number);
+
   console.log(calculator.userInput);
   console.log("length = " + calculator.userInput.length);
 }
 
-function refreshTextbox() {
-  var textbox = document.getElementById("textbox");
-
+function calculate() {
   for (var pos in calculator.userInput) {
     calculator.sum += calculator.userInput[pos];
   }
-  textbox.value = calculator.sum;
+  printSum();
   calculator.userInput = []; // emptying the array
   console.log("contents of array: " + calculator.userInput);
+}
+
+// displays sum in textbox
+function printSum() {
+
+  var textbox = document.getElementById("textbox");
+  textbox.value = calculator.sum;
+}
+
+function addToTextbox(number) {
+  var textbox = document.getElementById("textbox");
+  textbox.value += number;
 }
